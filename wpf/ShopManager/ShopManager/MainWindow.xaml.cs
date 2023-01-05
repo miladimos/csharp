24d15219329ec32d10c7e52700c5b579067f9c65
miladimos <milad.jafari6210@gmail.com>
2023-01-06 03:19:36 +0330

    wpf add shop management

---
+++ wpf/ShopManager/ShopManager/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ShopManager
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnHome_Click(object sender, RoutedEventArgs e)
+        {
+            HomePanel.Visibility = Visibility.Visible;
+            EmployeesPanel.Visibility = Visibility.Collapsed;
+            CustomersPanel.Visibility = Visibility.Collapsed;
+            ProductsPanel.Visibility = Visibility.Collapsed;    
+        }
+
+        private void btnEmployees_Click(object sender, RoutedEventArgs e)
+        {
+            HomePanel.Visibility = Visibility.Collapsed;
+            EmployeesPanel.Visibility = Visibility.Visible;
+            CustomersPanel.Visibility = Visibility.Collapsed;
+            ProductsPanel.Visibility = Visibility.Collapsed;
+        }
+
+        private void btnCustomers_Click(object sender, RoutedEventArgs e)
+        {
+            HomePanel.Visibility = Visibility.Collapsed;
+            EmployeesPanel.Visibility = Visibility.Collapsed;
+            CustomersPanel.Visibility = Visibility.Visible;
+            ProductsPanel.Visibility = Visibility.Collapsed;
+        }
+
+        private void btnProducts_Click(object sender, RoutedEventArgs e)
+        {
+            HomePanel.Visibility = Visibility.Collapsed;
+            EmployeesPanel.Visibility = Visibility.Collapsed;
+            CustomersPanel.Visibility = Visibility.Collapsed;
+            ProductsPanel.Visibility = Visibility.Visible;
+        }
+    }
+}
